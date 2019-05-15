15582d6ef2c6ee1d3d34fe505fa6aec2c13faedb
Darren Shepherd <darren@rancher.com>
2019-05-15 10:59:35 -0700

    Initial Commit

---
+++ pkg/codegen/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/rancher/wrangler-api/pkg/apis/some.api.group/v1"
+	"github.com/rancher/wrangler/pkg/controller-gen"
+	"github.com/rancher/wrangler/pkg/controller-gen/args"
+)
+
+func main() {
+	controllergen.Run(args.Options{
+		OutputPackage: "github.com/rancher/wrangler-api/pkg/generated",
+		Boilerplate:   "hack/boilerplate.go.txt",
+		Groups: map[string]args.Group{
+			"some.api.group": {
+				Types: []interface{}{
+					v1.Foo{},
+				},
+				GenerateTypes: true,
+			},
+		},
+	})
+}
